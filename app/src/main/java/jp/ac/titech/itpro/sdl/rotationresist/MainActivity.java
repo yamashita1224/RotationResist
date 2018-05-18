cbd978dd30411db36dd9c3ed8b0feafe4d77fa19
Takuo WATANABE <takuo@acm.org>
2018-05-18 09:40:15 +0900

    Now RotationView works

--- app/src/main/java/jp/ac/titech/itpro/sdl/rotationresist/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/rotationresist/MainActivity.java
@@ -1,13 +1,65 @@
 package jp.ac.titech.itpro.sdl.rotationresist;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+    private final static String TAG = "MainActivity";
+
+    private RotationView rotationView;
+
+    private SensorManager sensorManager;
+    private Sensor gyroscope;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+
+        rotationView = findViewById(R.id.rotation_view);
+
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        if (sensorManager == null) {
+            Toast.makeText(this, R.string.toast_no_sensor_manager, Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+        gyroscope = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        if (gyroscope == null) {
+            Toast.makeText(this, R.string.toast_no_gyroscope, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        sensorManager.registerListener(this, gyroscope, SensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        float omegaZ = event.values[2];  // z-axis angular velocity (radian / sec)
+        // TODO: calculate right direction that cancels the rotation
+        rotationView.setDirection(omegaZ);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        Log.d(TAG, "onAccuracyChanged: accuracy=" + accuracy);
     }
 }
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/rotationresist/RotationView.java
@@ -0,0 +1,56 @@
+package jp.ac.titech.itpro.sdl.rotationresist;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+
+class RotationView extends View {
+    private final static String TAG = "RotationView";
+
+    private double direction = 0;
+    private final Paint paint = new Paint();
+
+    public RotationView(Context context) {
+        this(context, null);
+    }
+
+    public RotationView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RotationView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        Log.d(TAG, "onSizeChanged: w=" + w + " h=" + h);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        int w = canvas.getWidth();
+        int h = canvas.getHeight();
+        int cx = w / 2, cy = h / 2;
+        int r = (cx < cy ? cx : cy) - 20;
+        paint.setColor(Color.LTGRAY);
+        canvas.drawCircle(cx, cy, r, paint);
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(cx, cy, 20, paint);
+        paint.setStrokeWidth(10);
+        float x = (float) (cx + r * Math.cos(direction));
+        float y = (float) (cy + r * Math.sin(direction));
+        canvas.drawLine(cx, cy, x, y, paint);
+    }
+
+    void setDirection(double th) {
+        direction = th - Math.PI / 2;
+        invalidate();
+    }
+}
